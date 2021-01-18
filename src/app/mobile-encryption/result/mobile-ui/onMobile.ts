8aeba227c5fbbd0d139ada523a4a6e4f8e8af11a
Dilshat Hewzulla <hewzulla@gmail.com>
2021-01-18 00:49:54 +0000

    mobile-encryption

---
+++ src/app/mobile-encryption/result/mobile-ui/onMobile.ts
@@ -0,0 +1,65 @@
+import { useMobile } from '../../../mobile';
+export const FIELDS = {
+    info: {
+        type: "info",
+        value: 'Select the field to copy it into your clipboard.'
+    },
+    content: {
+        id: "encryptedContent",
+        label: "Encrypted Content",
+        type: 'text',
+        nLines: 5,
+        value: ''
+    },
+
+    restart: {
+        id: "restart",
+        label: "Encrypt Again",
+        type: "button",
+        viewId: "row1"
+    },
+    finish: {
+        id: "finish",
+        label: "Finish",
+        type: "button",
+        viewId: "row1"
+    },
+};
+const initData = (content:string) => ({
+    form: {
+        title: "Content To Encrypt",
+        views: {
+            viewIds: {
+                row1:{
+                    style:{
+                        display:'flex',
+                        justifyContent:'space-between',
+                        width:'100%',
+                    }
+                }
+                                }
+        },
+        fields: [FIELDS.info, { ...FIELDS.content, value: content },  FIELDS.restart, FIELDS.finish]
+    }
+});
+interface UseConnectMobileProps{
+    content:string;
+    restart:()=>void;
+    finish:()=>void;
+}
+export const useConnectMobile = ({content,restart,finish}:UseConnectMobileProps) =>{
+    const mobile = useMobile(()=>initData(content), true);
+    mobile.setOnchange(({ field }) => {
+        switch (field.id) {
+            case FIELDS.restart.id:
+                restart();
+                break;
+            case FIELDS.finish.id:
+                finish();
+                break;
+
+            default:
+        }
+    });
+    return mobile;
+}
