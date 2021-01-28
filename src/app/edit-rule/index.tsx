b19b111c5f19eb63185dd9654d8653232f747811
Dilshat Hewzulla <hewzulla@gmail.com>
2021-01-28 11:05:56 +0000

    page-control-rule-edit

--- src/app/edit-rule/index.tsx
+++ src/app/edit-rule/index.tsx
@@ -1,9 +1,10 @@
-import React, { useState,useCallback } from 'react';
-import { useMobile } from '../mobile';
+import React, { useState } from 'react';
+
 
 import * as rules from '../page-control/rules';
-import {Form,Input,Label,Footer, DarkButton,Help,
-    DomainField,PopupWindow, TopBar,Content,Domain, Field, TextArea,Error} from '../components';
+import {Label,DarkButton, Field, TextArea,Error,NoMobilePage} from '../components';
+
+import {useConnectToMobile} from './mobile-ui';
 
 interface Props {
     back: () => void;
@@ -24,13 +25,6 @@ export const EditRule: React.FC<Props> = ({ back, domain,loadFromPreset }) => {
         setContent(content);
         back();
     };
-    const initData = () => ({
-        form: {
-            title: "Edit Rules",
-            fields: [{ ...FIELDS.domain, value: domain }, FIELDS.info, { ...FIELDS.editor, value: content }, FIELDS.cancel, FIELDS.save, FIELDS.loadFromPreset]
-        }
-    });
-    const mobile = useMobile(initData);
     const onSave = () => {
         try {
             content && JSON.parse(content);
@@ -41,39 +35,23 @@ export const EditRule: React.FC<Props> = ({ back, domain,loadFromPreset }) => {
         }
         saveRule(content);
     };
-    mobile.setOnchange(({field}) => {
-        switch (field.id) {
-            case FIELDS.cancel.id:
-                back();
-                break;
-            case FIELDS.editor.id:
-                setContent(field.value as string);
-                break;
-            case FIELDS.save.id:
-                onSave();
-                break;
-            case FIELDS.loadFromPreset.id:
-                loadFromPreset();
-                break;
-            default:
-        }
-    });
-    const { sendValue } = mobile;
-    const onContentChange = useCallback((content: string) => {
+    const {mobile, sendContent } = useConnectToMobile(domain,content, back,setContent,onSave,loadFromPreset);
+
+
+    const  onContentChange = (content: string) => {
         setContent(content);
-        sendValue(FIELDS.editor.id, content);
-    }, [sendValue]);
+        sendContent(content);
+    }
 
-
+    const footer=(
+        <>
+            <DarkButton onClick={back}>Cancel</DarkButton>
+            <DarkButton onClick={loadFromPreset}>Use Preset</DarkButton>
+            <DarkButton onClick={onSave}>Save</DarkButton>
+        </>
+    );
     return (
-    <PopupWindow>
-        <TopBar>
-            Edit Rules
-        </TopBar>
-    <Content>
-        <Domain>{domain}</Domain>
-        <Form>
-
+    <NoMobilePage title="Edit Rules" domain={domain} footer={footer}>
             <Field>
                 <TextArea id="ruleContent"
                 value={content}
@@ -86,62 +64,9 @@ export const EditRule: React.FC<Props> = ({ back, domain,loadFromPreset }) => {
             </Field>
                 {errorMessage && (
             <Error>{errorMessage}errorMessage</Error>)}
-
-
-
-</Form>
-
-</Content>
-<Footer>
-            <DarkButton onClick={back}>Cancel</DarkButton>
-            <DarkButton onClick={loadFromPreset}>Use Preset</DarkButton>
-            <DarkButton onClick={onSave}>Save</DarkButton>
-        </Footer>
-
-</PopupWindow>
+      </NoMobilePage>
 );
 
 
 
 }
-
-
-
-
-const FIELDS = {
-    domain: {
-        type: 'info',
-        value: '',
-    },
-    info: {
-        id: 'info',
-        type: "info",
-        value: "Please operate in the extension window on your computer",
-        viewId: "row3"
-    },
-    editor: {
-        id: 'editor',
-        type: 'text',
-        nLines: 5,
-        value: '',
-        viewId: "row3"
-    },
-    cancel: {
-        id: "cancelEdit",
-        type: "button",
-        label: "Cancel",
-        viewId: "row4"
-    },
-    save: {
-        id: "saveEdit",
-        type: "button",
-        label: "Save",
-        viewId: "row4"
-    },
-    loadFromPreset: {
-        id: "load-from-preset",
-        type: "button",
-        label: "Use Preset",
-        viewId: "row5"
-    }
-};
---
+++ src/app/edit-rule/mobile-ui/index.ts
@@ -0,0 +1,74 @@
+import { useMobile } from '../../mobile';
+
+const FIELDS = {
+    domain: {
+        type: 'info',
+        value: '',
+    },
+    info: {
+        id: 'info',
+        type: "info",
+        value: "Please operate in the extension window on your computer",
+        viewId: "row3"
+    },
+    editor: {
+        id: 'editor',
+        type: 'text',
+        nLines: 5,
+        value: '',
+        viewId: "row3"
+    },
+    cancel: {
+        id: "cancelEdit",
+        type: "button",
+        label: "Cancel",
+        viewId: "row4"
+    },
+    save: {
+        id: "saveEdit",
+        type: "button",
+        label: "Save",
+        viewId: "row4"
+    },
+    loadFromPreset: {
+        id: "load-from-preset",
+        type: "button",
+        label: "Use Preset",
+        viewId: "row5"
+    }
+};
+
+    const initData = (domain:string, content:string) => ({
+        form: {
+            title: "Edit Rules",
+            fields: [{ ...FIELDS.domain, value: domain }, FIELDS.info, { ...FIELDS.editor, value: content },
+                FIELDS.cancel, FIELDS.save, FIELDS.loadFromPreset]
+        }
+    });
+
+export const useConnectToMobile=(domain:string, content:string, back:()=>void, setContent:(content:string)=>void,onSave:()=>void, loadFromPreset:()=>void)=>{
+    const mobile = useMobile(()=>initData(domain, content));
+    mobile.setOnchange(({field}) => {
+        switch (field.id) {
+            case FIELDS.cancel.id:
+                back();
+                break;
+            case FIELDS.editor.id:
+                setContent(field.value as string);
+                break;
+            case FIELDS.save.id:
+                onSave();
+                break;
+            case FIELDS.loadFromPreset.id:
+                loadFromPreset();
+                break;
+            default:
+        }
+    });
+    const { sendValue } = mobile;
+
+    const sendContent = (content:string)=>{
+        sendValue(FIELDS.editor.id, content);
+    };
+    return {mobile,sendContent};
+}
